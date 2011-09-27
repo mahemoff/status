b6512bbc7f064df75650a6aa1db6bc5e836243a6
Mahemoff <michael@mahemoff.com>
2011-09-27 16:10:37 +0100

    add doco

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# We use a tiny python server because in-browser CoffeeScript won't work
+# on file:/// URIs.
 echo 'please surf to http://localhost:8000/examples/index.html'
 python -m SimpleHTTPServer
